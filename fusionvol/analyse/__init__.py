9ca2caeb25e826de76af9fb428390ef348d415da
YannBouroullec <yann.bouroullec@edu.ece.fr>
2026-06-15 17:27:40 +0200

    chore: exposer le sous-paquet analyse

---
+++ fusionvol/analyse/__init__.py
@@ -0,0 +1,6 @@
+"""Sous-paquet de détection de phases et d'analyse de performance."""
+
+from fusionvol.analyse.phases import DetecteurPhases, Phases
+from fusionvol.analyse.performance import AnalysePerformance
+
+__all__ = ["DetecteurPhases", "Phases", "AnalysePerformance"]
